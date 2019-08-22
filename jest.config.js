7b10cbba71a30a1a80830bc2f8ba0abb9fcfcd6e
PercyPham <hungpm.percy@gmail.com>
2019-08-22 10:35:10 +0700

    chore: tidy up

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,6 @@
 module.exports = {
 	coverageDirectory: './.coverage',
-	modulePathIgnorePatterns: [
-		'<rootDir>[/\\\\](scripts|build|docs|public)[/\\\\]'
-	],
+	modulePathIgnorePatterns: ['<rootDir>[/\\\\](scripts|build|docs|public)[/\\\\]'],
 	testMatch: ['<rootDir>/**/__tests__/*.test.js'],
 	testPathIgnorePatterns: ['<rootDir>/node_modules/']
 };
