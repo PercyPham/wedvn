5177ccff2a2b91b412cf09dd26005edfbd2e1b22
PercyPham <hungpm.percy@gmail.com>
2019-08-27 12:38:06 +0700

    fix: add prod ip to known_hosts on travis-ci

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # This script are meant to run at the root of project
 
+if [ -z `ssh-keygen -F $PROD_IP` ]; then
+  ssh-keyscan -H $PROD_IP >> ~/.ssh/known_hosts
+fi
+
 eval "$(ssh-agent -s)"
 chmod 600 deploy-key-id_rsa
 ssh-add deploy-key-id_rsa
